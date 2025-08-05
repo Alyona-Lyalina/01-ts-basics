430b552adcc206f0be0cba45ffdf5d0fbb10f434
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-08-05 21:55:26 +0300

    fix error

--- src/task-6.ts
+++ src/task-6.ts
@@ -7,4 +7,4 @@ function getFirstElement<T>(arr: T[]): T {
 console.log(getFirstElement<number>([1, 2, 3]));
 console.log(getFirstElement<string>(["a", "b", "c"]));
 console.log(getFirstElement<boolean>([true, false, true]));
-console.log(getFirstElement([1, "a", true]));
+console.log(getFirstElement<string | number | boolean>([1, "a", true]));
